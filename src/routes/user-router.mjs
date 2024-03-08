e406f4b3a3ddef839b7e1583af7dfcd6ebee4de4
eeroelo <111982645+eeroelo@users.noreply.github.com>
2024-03-08 15:05:03 +0200

    Update user-router.mjs

--- src/routes/user-router.mjs
+++ src/routes/user-router.mjs
@@ -1,6 +1,5 @@
 import express from 'express';
-import bcrypt from 'bcryptjs';
-import { body } from 'express-validator';
+import {body, param} from 'express-validator';
 import {
   getUserById,
   getUsers,
@@ -8,37 +7,81 @@ import {
   putUser,
   deleteUser,
 } from '../controllers/user-controller.mjs';
-import { authenticateToken } from '../middlewares/authentication.mjs';
+import {authenticateToken} from '../middlewares/authentication.mjs';
+import {validationErrorHandler} from '../middlewares/error-handler.mjs';
 
 const userRouter = express.Router();
 
-// Error handling middleware
-const errorHandler = (err, req, res, next) => {
-  const statusCode = err.status || 500;
-  const message = err.message || 'Internal Server Error';
-  console.error(err);
-  res.status(statusCode).json({ error: message });
-};
-
-// Attach error handling middleware to the router
-userRouter.use(errorHandler);
-
-// /user endpoint
+// /api/user endpoint
 userRouter
   .route('/')
+  /**
+   * @api {get} /users Request user list
+   * @apiName GetUsers
+   * @apiGroup Users
+   * @apiPermission token
+   *
+   * @apiSuccess {Array} user[] array of Users.
+   * @apiSuccess {Object} user User object.
+   * @apiSuccess {Number} user.user_id Id of the user.
+   * @apiSuccess {String} user.username Username
+   * @apiSuccess {String} user.user_level Userlevel of the User.
+   *
+   * TODO: add example response
+   *
+   * @apiUse InvalidTokenError
+   */
   .get(authenticateToken, getUsers)
-  .put(authenticateToken, putUser)
+  // update user
+  .put(
+    authenticateToken,
+    body('username', 'username must be 3-20 characters long and alphanumeric')
+      .trim()
+      .isLength({min: 3, max: 20})
+      .isAlphanumeric(),
+    body('password', 'minimum password length is 8 characters')
+      .trim()
+      .isLength({min: 8, max: 128}),
+    body('email', 'must be a valid email address')
+      .trim()
+      .isEmail()
+      .normalizeEmail(),
+    validationErrorHandler,
+    putUser,
+  )
+  // user registration
   .post(
-    body('username').trim().isLength({ min: 3, max: 20 }).isAlphanumeric(),
-    body('password').trim().isLength({ min: 8, max: 128 }),
-    body('email').trim().isEmail(),
-    postUser
+    body('username', 'username must be 3-20 characters long and alphanumeric')
+      .trim()
+      .isLength({min: 3, max: 20})
+      .isAlphanumeric(),
+    body('password', 'minimum password length is 8 characters')
+      .trim()
+      .isLength({min: 8, max: 128}),
+    body('email', 'must be a valid email address')
+      .trim()
+      .isEmail()
+      .normalizeEmail(),
+    validationErrorHandler,
+    postUser,
   );
 
 // /user/:id endpoint
 userRouter
   .route('/:id')
-  .get(authenticateToken, getUserById)
-  .delete(authenticateToken, deleteUser);
+  // get info of a user
+  .get(
+    authenticateToken,
+    param('id', 'must be integer').isInt(),
+    validationErrorHandler,
+    getUserById,
+  )
+  // delete user based on id
+  .delete(
+    authenticateToken,
+    param('id', 'must be integer').isInt(),
+    validationErrorHandler,
+    deleteUser,
+  );
 
 export default userRouter;
