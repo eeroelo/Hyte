0828fab181c2a21c092ed5c7a8995ba8bd0861f1
eeroelo <111982645+eeroelo@users.noreply.github.com>
2024-03-08 15:31:44 +0200

    Update user-controller.mjs

--- src/controllers/user-controller.mjs
+++ src/controllers/user-controller.mjs
@@ -1,5 +1,4 @@
 import bcrypt from 'bcryptjs';
-import { validationResult } from 'express-validator';
 import {
   deleteUserById,
   insertUser,
@@ -7,87 +6,75 @@ import {
   selectUserById,
   updateUserById,
 } from '../models/user-model.mjs';
+import {customError} from '../middlewares/error-handler.mjs';
 
-// Error handling middleware
-const errorHandler = (err, req, res, next) => {
-  // Handle specific errors or set a default status code
-  const statusCode = err.status || 500;
-  const message = err.message || 'Internal Server Error';
-  console.error(err);
-  res.status(statusCode).json({ error: message });
-};
-
-const getUsers = async (req, res) => {
+const getUsers = async (req, res, next) => {
   const result = await listAllUsers();
   if (result.error) {
-    throw new Error(result.error);
+    return next(customError(result, result.error));
   }
   return res.json(result);
 };
 
-const getUserById = async (req, res) => {
+const getUserById = async (req, res, next) => {
   const result = await selectUserById(req.params.id);
   if (result.error) {
-    throw new Error(result.error);
+    return next(customError(result, result.error));
   }
   return res.json(result);
 };
 
 const postUser = async (req, res, next) => {
-  const { username, password, email } = req.body;
-  const validationErrors = validationResult(req);
-  if (!validationErrors.isEmpty()) {
-    const error = new Error('Validation failed');
-    error.status = 422;
-    error.errors = validationErrors.array();
-    throw error;
-  }
-
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    const result = await insertUser({
+  const {username, password, email} = req.body;
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const result = await insertUser(
+    {
       username,
       email,
       password: hashedPassword,
-    }, next);
-    return res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
+    },
+    next,
+  );
+  return res.status(201).json(result);
 };
 
 const putUser = async (req, res, next) => {
+  // Get userinfo from req.user object extracted from token
+  // Only user authenticated by token can update own data
+  // TODO: admin user can update any user (incl. user_level)
   const userId = req.user.user_id;
-  const { username, password, email } = req.body;
+  const {username, password, email} = req.body;
+  // hash password if included in request
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
-
-  if (userId && username && password && email) {
-    try {
-      const result = await updateUserById({
-        userId,
-        username,
-        password: hashedPassword,
-        email,
-      });
-      return res.status(200).json(result);
-    } catch (error) {
-      next(error);
-    }
-  } else {
-    const error = new Error('Bad request');
-    error.status = 400;
-    throw error;
+  const result = await updateUserById({
+    userId,
+    username,
+    password: hashedPassword,
+    email,
+  });
+  if (result.error) {
+    return next(customError(result, result.error));
   }
+  return res.status(200).json(result);
 };
 
-const deleteUser = async (req, res) => {
+const deleteUser = async (req, res, next) => {
+  // console.log('deleteUser', req.user, req.params.id);
+  // admin user can delete any user
+  // user authenticated by token can delete itself
+  if (
+    req.user.user_level !== 'admin' &&
+    req.user.user_id !== parseInt(req.params.id)
+  ) {
+    return next(customError('Unauthorized', 401));
+  }
   const result = await deleteUserById(req.params.id);
   if (result.error) {
-    throw new Error(result.error);
+    return next(customError(result, result.error));
   }
   return res.json(result);
 };
 
-export { getUsers, getUserById, postUser, putUser, deleteUser, errorHandler };
+export {getUsers, getUserById, postUser, putUser, deleteUser};
